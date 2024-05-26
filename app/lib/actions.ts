1db71f29f73ffa9b83ccac276243e1b98932c47e
Nottnt <tanatat19@outlook.com>
2024-05-26 16:54:56 +0700

    feat: adjust params for server action to json format

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,13 +14,13 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function createInvoice(formData: FormData) {
-  console.log('formData', formData)
+export async function createInvoice(payload: any) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: payload.customerId,
+    amount: payload.amount,
+    status: payload.status,
   })
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
@@ -32,4 +32,28 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, payload: any) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: payload.customerId,
+    amount: payload.amount,
+    status: payload.status,
+  })
+
+  const amountInCents = amount * 100
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
 }
